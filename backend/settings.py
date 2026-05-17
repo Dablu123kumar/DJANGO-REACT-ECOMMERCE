11916e20ba7a78c632e736959a1c8b3eed63b842
Dablu123kumar <krishnaroysilaur@gmail.com>
2026-05-17 14:58:08 +0530

    Remove cloudinary_storage from INSTALLED_APPS to fix collectstatic override bug

--- backend/settings.py
+++ backend/settings.py
@@ -19,7 +19,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     # Third party
